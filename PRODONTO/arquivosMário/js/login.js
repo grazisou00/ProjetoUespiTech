23a6cd18f504fda951852057d52282842340f939
Graziela Souza <118382634+grazisou00@users.noreply.github.com>
2024-07-12 00:03:43 -0300

    Update login.js

--- PRODONTO/arquivosMário/js/login.js
+++ PRODONTO/arquivosMário/js/login.js
@@ -1,9 +1,16 @@
-function abrirModalVm(){
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
+function abrirModalVm() {
+    const modal = document.getElementById('janela-modal');
+    modal.classList.add('abrir');
 
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar' || e.target.id == 'janela-modal')
-            modal.classList.remove('abrir')
-    })
+    function fecharModal(e) {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal') {
+            modal.classList.remove('abrir');
+            modal.removeEventListener('click', fecharModal);
+        }
+    }
+
+    modal.addEventListener('click', fecharModal);
+}
+function redirecionarPagina() {
+    window.location.href = "http://127.0.0.1:5500/PRODONTO/arquivosM%C3%A1rio/html/areadeEstudo%20(1).html"; // Substitua "URL_DA_PAGINA" pela URL para onde você deseja redirecionar
 }
