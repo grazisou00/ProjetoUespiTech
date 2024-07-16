119e1dbd80d6508fa17540d393068d93f54e0d22
mariogomess2312 <165229408+mariogomess2312@users.noreply.github.com>
2024-07-15 21:53:49 -0300

    Update incisivoSuperiores.js

--- PRODONTO/arquivosMário/js/incisivoSuperiores.js
+++ PRODONTO/arquivosMário/js/incisivoSuperiores.js
@@ -1,3 +1,21 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('.menu-desktop').classList.toggle('active');
-});
+function abrirModal(modalId) {
+    document.getElementById(modalId).classList.add("abrir");
+  }
+  
+  function fecharModal(modalId) {
+    document.getElementById(modalId).classList.remove("abrir");
+  }
+  
+  // Abrir e fechar cada modal
+  document.querySelector('.button-1').onclick = () => abrirModal('modal-sutura-palatina-mediana');
+  document.querySelector('.button-2').onclick = () => abrirModal('modal-fossas-nasais');
+  document.querySelector('.button-3').onclick = () => abrirModal('modal-forame-incisivo');
+  document.querySelector('.button-4').onclick = () => abrirModal('modal-canal-incisivo');
+  document.querySelector('.button-5').onclick = () => abrirModal('modal-aberturas-nasais-canal-incisivo');
+  document.querySelector('.button-6').onclick = () => abrirModal('modal-fossas-incisivas');
+  document.querySelector('.button-7').onclick = () => abrirModal('modal-aberturas-nasais-anterior');
+  document.querySelector('.button-8').onclick = () => abrirModal('modal-assoalho-fossas-nasais');
+  document.querySelector('.button-9').onclick = () => abrirModal('modal-conchas-nasais-inferiores');
+  document.querySelector('.button-10').onclick = () => abrirModal('modal-septo-nasal');
+  document.querySelector('.button-11').onclick = () => abrirModal('modal-paredes-canal-incisivo');
+  
