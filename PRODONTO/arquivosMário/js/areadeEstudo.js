38b62a8b10fa28a1eddfa6694530882b69a8bf79
mariogomess2312 <165229408+mariogomess2312@users.noreply.github.com>
2024-07-19 14:37:58 -0300

    Revert "Update areadeEstudo.js"
    This reverts commit 04be13826ca9906997f0734fb67d3e0f86462c71.

--- PRODONTO/arquivosMário/js/areadeEstudo.js
+++ PRODONTO/arquivosMário/js/areadeEstudo.js
@@ -1,13 +1,31 @@
-function abrirModalVm() {
-    const modal = document.getElementById('janela-modal');
-    modal.classList.add('abrir');
 
-    function fecharModal(e) {
-        if (e.target.id == 'fechar' || e.target.id == 'janela-modal') {
-            modal.classList.remove('abrir');
-            modal.removeEventListener('click', fecharModal);
-        }
-    }
-
-    modal.addEventListener('click', fecharModal);
+function redirecionarPagina() {
+    window.location.href = ""; 
+}
+function redirecionarPagina() {
+    window.location.href = "";
+}
+function redirecionarPagina() {
+    window.location.href = "";
+}
+function redirecionarPagina() {
+    window.location.href = "";
+}
+function redirecionarPagina() {
+    window.location.href = ""; 
+}
+function redirecionarPagina() {
+    window.location.href = "";
+}
+function redirecionarPagina() {
+    window.location.href = "";
+}
+function redirecionarPagina() {
+    window.location.href = "";
+}
+function redirecionarPagina() {
+    window.location.href = "";
+}
+function redirecionarPagina() {
+    window.location.href = "";
 }
