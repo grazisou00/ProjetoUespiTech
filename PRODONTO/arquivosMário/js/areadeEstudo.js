16b7650bc9cdda7acc19b6bd638da4d6220cea4d
mariogomess2312 <165229408+mariogomess2312@users.noreply.github.com>
2024-07-19 14:53:09 -0300

    Reapply "Update areadeEstudo.js"
    This reverts commit 38b62a8b10fa28a1eddfa6694530882b69a8bf79.

--- PRODONTO/arquivosMário/js/areadeEstudo.js
+++ PRODONTO/arquivosMário/js/areadeEstudo.js
@@ -1,31 +1,13 @@
+function abrirModalVm() {
+    const modal = document.getElementById('janela-modal');
+    modal.classList.add('abrir');
 
-function redirecionarPagina() {
-    window.location.href = ""; 
-}
-function redirecionarPagina() {
-    window.location.href = "";
-}
-function redirecionarPagina() {
-    window.location.href = "";
-}
-function redirecionarPagina() {
-    window.location.href = "";
-}
-function redirecionarPagina() {
-    window.location.href = ""; 
-}
-function redirecionarPagina() {
-    window.location.href = "";
-}
-function redirecionarPagina() {
-    window.location.href = "";
-}
-function redirecionarPagina() {
-    window.location.href = "";
-}
-function redirecionarPagina() {
-    window.location.href = "";
-}
-function redirecionarPagina() {
-    window.location.href = "";
+    function fecharModal(e) {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal') {
+            modal.classList.remove('abrir');
+            modal.removeEventListener('click', fecharModal);
+        }
+    }
+
+    modal.addEventListener('click', fecharModal);
 }
