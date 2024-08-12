51dcaf3ae1cef7a1eb0601e4dfeb87b314c9c851
mariogomess2312 <165229408+mariogomess2312@users.noreply.github.com>
2024-08-12 20:48:23 -0300

    Update caninosSuperiores.js

--- PRODONTO/arquivosMário/js/caninosSuperiores.js
+++ PRODONTO/arquivosMário/js/caninosSuperiores.js
@@ -10,50 +10,89 @@ function abrirModalVm() {
     }
     modal.addEventListener('click', fecharModal);
 }
+const modals = [
 
+    "modal-seio-maxilar",
+    "modal-fossa-nasal",
+    "modal-fossa-canina",
+    "modal-assoalho-do-seio-maxilar",
+    "modal-assoalho-da-fossa-nasal",
+    "modal-y-invertido-de-ennis"
+];
+
+let currentModalIndex = 0;
 let originalImage = '/PRODONTO/img/img223.jpeg';
 let compareImage = '/PRODONTO/img/img224.jpeg';
 
-function abrirModal() {
-    const modal = document.getElementById('janela-modal-estrutura');
-    modal.classList.add('abrir');
-    modal.addEventListener('click', (e) => {
-        if (e.target.id == 'close-btn' || e.target.id == 'janela-modal-estrutura') {
-            modal.classList.remove('abrir')
+function abrirModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.add("abrir");
+
+    const closeModal = () => {
+        modal.classList.remove("abrir");
+    };
+
+    modal.querySelector(".close-btn").addEventListener("click", closeModal);
+    modal.addEventListener("click", (e) => {
+        if (e.target.classList.contains('janela-modal-estrutura')) {
+            closeModal();
         }
-    })
+    });
+
+    // Atualizar o índice do modal atual
+    currentModalIndex = modals.indexOf(modalId);
+
+    // Adicionar event listeners ao modal atualmente aberto
+    const modalImage = modal.querySelector('#modal-image');
+    if (modalImage) {
+        modalImage.addEventListener('mouseover', function () {
+            this.src = compareImage;
+        });
+
+        modalImage.addEventListener('mouseout', function () {
+            this.src = originalImage;
+        });
+
+        modalImage.addEventListener('click', function () {
+            let temp = originalImage;
+            originalImage = compareImage;
+            compareImage = temp;
+            this.src = originalImage;
+        });
+    }
 }
-//function closeModal() {
-//document.querySelector('.modal-estrutura').style.display = 'none';
-//}
+
+function previousModal() {
+    const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
+    if (currentModal) {
+        currentModal.classList.remove('abrir');
+    }
+
+    currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
+    const previousModalId = modals[currentModalIndex];
+    abrirModal(previousModalId);
+}
+
+function nextModal() {
+    const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
+    if (currentModal) {
+        currentModal.classList.remove('abrir');
+    }
+
+    currentModalIndex = (currentModalIndex + 1) % modals.length;
+    const nextModalId = modals[currentModalIndex];
+    abrirModal(nextModalId);
+}
+
+// Abrir e fechar cada modal
+document.querySelector('.button-1').onclick = () => abrirModal('modal-seio-maxilar');
+document.querySelector('.button-2').onclick = () => abrirModal('modal-fossa-nasal');
+document.querySelector('.button-3').onclick = () => abrirModal('modal-fossa-canina');
+document.querySelector('.button-4').onclick = () => abrirModal('modal-assoalho-do-seio-maxilar');
+document.querySelector('.button-5').onclick = () => abrirModal('modal-assoalho-da-fossa-nasal');
+document.querySelector('.button-6').onclick = () => abrirModal('modal-y-invertido-de-ennis');
 
 function changeImage(img1, img2) {
     originalImage = img1;
     compareImage = img2;
-    document.getElementById('modal-image').src = originalImage;
-}
-
-document.getElementById('modal-image').addEventListener('mouseover', function () {
-    this.src = compareImage;
-});
-
-document.getElementById('modal-image').addEventListener('mouseout', function () {
-    this.src = originalImage;
-});
-
-document.getElementById('modal-image').addEventListener('click', function () {
-    let temp = originalImage;
-    originalImage = compareImage;
-    compareImage = temp;
-    this.src = originalImage;
-});
-
-function previousModal() {
-    // Implement your logic to show the previous modal
-    alert("Previous modal");
-}
-
-function nextModal() {
-    // Implement your logic to show the next modal
-    alert("Next modal");
 }
