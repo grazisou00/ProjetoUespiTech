1b508c94fa39eb7bf76979ed3eea8ed5bb45f313
mariogomess2312 <165229408+mariogomess2312@users.noreply.github.com>
2024-08-12 20:48:20 -0300

    Update caninosInferiores.js

--- PRODONTO/arquivosMário/js/caninosInferiores.js
+++ PRODONTO/arquivosMário/js/caninosInferiores.js
@@ -11,3 +11,84 @@ function abrirModalVm() {
 
     modal.addEventListener('click', fecharModal);
 }
+const modals = [
+
+    'modal-forame-mentual',
+    'modal-base-mandibular',
+    'modal-protuberncia-mentual'
+
+];
+
+let currentModalIndex = 0;
+let originalImage = '/PRODONTO/img/img223.jpeg';
+let compareImage = '/PRODONTO/img/img224.jpeg';
+
+function abrirModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.add("abrir");
+
+    const closeModal = () => {
+        modal.classList.remove("abrir");
+    };
+
+    modal.querySelector(".close-btn").addEventListener("click", closeModal);
+    modal.addEventListener("click", (e) => {
+        if (e.target.classList.contains('janela-modal-estrutura')) {
+            closeModal();
+        }
+    });
+
+    // Atualizar o índice do modal atual
+    currentModalIndex = modals.indexOf(modalId);
+
+    // Adicionar event listeners ao modal atualmente aberto
+    const modalImage = modal.querySelector('#modal-image');
+    if (modalImage) {
+        modalImage.addEventListener('mouseover', function () {
+            this.src = compareImage;
+        });
+
+        modalImage.addEventListener('mouseout', function () {
+            this.src = originalImage;
+        });
+
+        modalImage.addEventListener('click', function () {
+            let temp = originalImage;
+            originalImage = compareImage;
+            compareImage = temp;
+            this.src = originalImage;
+        });
+    }
+}
+
+function previousModal() {
+    const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
+    if (currentModal) {
+        currentModal.classList.remove('abrir');
+    }
+
+    currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
+    const previousModalId = modals[currentModalIndex];
+    abrirModal(previousModalId);
+}
+
+function nextModal() {
+    const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
+    if (currentModal) {
+        currentModal.classList.remove('abrir');
+    }
+
+    currentModalIndex = (currentModalIndex + 1) % modals.length;
+    const nextModalId = modals[currentModalIndex];
+    abrirModal(nextModalId);
+}
+
+// Abrir e fechar cada modal
+document.querySelector('.button-1').onclick = () => abrirModal('modal-forame-mentual');
+document.querySelector('.button-2').onclick = () => abrirModal('modal-base-mandibular');
+document.querySelector('.button-3').onclick = () => abrirModal('modal-protuberncia-mentual');
+
+function changeImage(img1, img2) {
+    originalImage = img1;
+    compareImage = img2;
+}
