afa47d177e55894962b54b731530e377279b16c6
Graziela Souza <118382634+grazisou00@users.noreply.github.com>
2024-09-01 01:48:03 -0300

    Update login.js

--- PRODONTO/arquivosMário/js/login.js
+++ PRODONTO/arquivosMário/js/login.js
@@ -14,3 +14,15 @@ function abrirModalVm() {
 function redirecionarPagina() {
     window.location.href = "http://127.0.0.1:5500/PRODONTO/arquivosM%C3%A1rio/html/areadeEstudo%20(1).html"; // Substitua "URL_DA_PAGINA" pela URL para onde você deseja redirecionar
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const logoAnimation = document.querySelector(".logo-animation");
+    const headerLogo = document.querySelector("header .logo img");
+
+    setTimeout(() => {
+        logoAnimation.classList.add("hidden");
+    }, 400);
+
+    logoAnimation.addEventListener("transitionend", () => {
+        logoAnimation.classList.add("hidden-logo");
+    });
+});
