a8587c555367076b3a89623e8cedb4596c2433ae
Graziela Souza <118382634+grazisou00@users.noreply.github.com>
2024-09-08 01:12:12 -0300

    Update molarSuperior.js

--- PRODONTO/arquivosMário/js/molarSuperior.js
+++ PRODONTO/arquivosMário/js/molarSuperior.js
@@ -10,70 +10,118 @@ const modals = [
     'modal-assoalho-da-fossa-nasal',
     'modal-tber-maxilar'
 ];
-
-let currentModalIndex = 0;
-let originalImage = '/PRODONTO/img/img223.jpeg';
-let compareImage = '/PRODONTO/img/img224.jpeg';
-
-function abrirModal(modalId) {
+const modal123 = [
+    {
+        id: 'modal-seio-maxilar',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar/assm-seio.jpeg', '/PRODONTO/img/MolareSuperior/Seio\ Maxilar.png']
+    },
+    {
+        id: 'modal-fossa-nasal',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar/fossa-assfn.jpeg', '/PRODONTO/img/MolareSuperior/Fossa\ Nasal.png']
+    },
+    {
+        id: 'modal-processo-zigomtico-da-maxila',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar/osszig-prcoro1-przimax.jpeg', '/PRODONTO/img/MolareSuperior/Processo\ Zigomático\ da\ Maxila.png']
+    },
+    {
+        id: 'modal-osso-zigomtico',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar/osszig-prcoro1-przimax.jpeg', '/PRODONTO/img/MolareSuperior/Osso\ Zigomático.png']
+    },
+    {
+        id: 'modal-processo-pterigideo',
+        images: ['', '/PRODONTO/img/MolareSuperior/Processo\ Pterigoide.png']
+    },
+    {
+        id: 'modal-hmulo-pterigideo',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar/ham-prcoro2.jpeg', '/PRODONTO/img/areadeEstudo/MolaresSuperiores/HamuloPterigoideo.png']
+    },
+    {
+        id: 'modal-processo-coronide-da-mandbula',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar/osszig-prcoro1-przimax.jpeg', '/PRODONTO/img/MolareSuperior/Processo\ Coronóide.png']
+    },
+    {
+        id: 'modal-assoalho-do-seio-maxilar',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar//assm-seio.jpeg', '/PRODONTO/img/MolareSuperior/Assoalho\ do\ Seio\ Maxilar.png']
+    },
+    {
+        id: 'modal-assoalho-da-fossa-nasal',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar/fossa-assfn.jpeg', '/PRODONTO/img/MolareSuperior/Assoalho\ da\ Fossa\ Nasal.png']
+    },
+    {
+        id: 'modal-tber-maxilar',
+        images: ['/PRODONTO/img/MolareSuperior/mosupmar/tuber.jpeg', '/PRODONTO/img/MolareSuperior/Túber\ da\ Maxila.png']
+    }
+  ];
+  
+  
+  let currentModalIndex = 0;
+  
+  function abrirModal(modalId) {
+      const modal = document.getElementById(modalId);
+      modal.classList.add("abrir");
+  
+      const closeModal = () => {
+          modal.classList.remove("abrir");
+      };
+  
+      modal.querySelector(".close-btn").addEventListener("click", closeModal);
+      modal.addEventListener("click", (e) => {
+          if (e.target.classList.contains('janela-modal-estrutura')) {
+              closeModal();
+          }
+      });
+  
+  
+      // Adicionar event listeners ao modal atualmente aberto
+      currentModalIndex = modals.indexOf(modalId);
+      const modalImage = modal.querySelector('img');
+      let [originalImage, compareImage] = modal123[currentModalIndex].images;
+      modalImage.src = originalImage;
+  
+      // Adicionar event listeners ao modal atualmente aberto
+      modalImage.addEventListener('mouseover', function () {
+          this.src = compareImage;
+      });
+  
+      modalImage.addEventListener('mouseout', function () {
+          this.src = originalImage;
+      });
+  
+      modalImage.addEventListener('click', function () {
+          let temp = originalImage;
+          originalImage = compareImage;
+          compareImage = temp;
+          this.src = originalImage;
+      });
+  }
+  function changeImage(modalId, imageIndex) {
     const modal = document.getElementById(modalId);
-    modal.classList.add("abrir");
-
-    const closeModal = () => {
-        modal.classList.remove("abrir");
-    };
-
-    modal.querySelector(".close-btn").addEventListener("click", closeModal);
-    modal.addEventListener("click", (e) => {
-        if (e.target.classList.contains('janela-modal-estrutura')) {
-            closeModal();
-        }
-    });
-
-    // Atualizar o índice do modal atual
-    currentModalIndex = modals.indexOf(modalId);
-
-    // Adicionar event listeners ao modal atualmente aberto
-    const modalImage = modal.querySelector('#modal-image');
-    if (modalImage) {
-        modalImage.addEventListener('mouseover', function () {
-            this.src = compareImage;
-        });
-
-        modalImage.addEventListener('mouseout', function () {
-            this.src = originalImage;
-        });
-
-        modalImage.addEventListener('click', function () {
-            let temp = originalImage;
-            originalImage = compareImage;
-            compareImage = temp;
-            this.src = originalImage;
-        });
-    }
-}
-
-function previousModal() {
-    const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
-    if (currentModal) {
-        currentModal.classList.remove('abrir');
-    }
-
-    currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
-    const previousModalId = modals[currentModalIndex];
-    abrirModal(previousModalId);
-}
-
-function nextModal() {
-    const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
-    if (currentModal) {
-        currentModal.classList.remove('abrir');
-    }
-
-    currentModalIndex = (currentModalIndex + 1) % modals.length;
-    const nextModalId = modals[currentModalIndex];
-    abrirModal(nextModalId);
-}
+    const modalImage = modal.querySelector('img');
+    const images = modal123.find(modalItem => modalItem.id === modalId).images;
+    modalImage.src = images[imageIndex];
+  }
+  
+  function previousModal() {
+      const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
+      if (currentModal) {
+          currentModal.classList.remove('abrir');
+      }
+  
+      currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
+      const previousModalId = modals[currentModalIndex];
+      abrirModal(previousModalId);
+  }
+  
+  function nextModal() {
+      const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
+      if (currentModal) {
+          currentModal.classList.remove('abrir');
+      }
+  
+      currentModalIndex = (currentModalIndex + 1) % modals.length;
+      const nextModalId = modals[currentModalIndex];
+      abrirModal(nextModalId);
+  }
 
 // Abrir e fechar cada modal
 document.querySelector('.button-1').onclick = () => abrirModal('modal-seio-maxilar');
@@ -87,10 +135,6 @@ document.querySelector('.button-8').onclick = () => abrirModal('modal-assoalho-d
 document.querySelector('.button-9').onclick = () => abrirModal('modal-assoalho-da-fossa-nasal');
 document.querySelector('.button-10').onclick = () => abrirModal('modal-tber-maxilar');
 
-function changeImage(img1, img2) {
-    originalImage = img1;
-    compareImage = img2;
-}
 function abrirModalVm() {
     const modal = document.getElementById('janela-modal');
     modal.classList.add('abrir');
