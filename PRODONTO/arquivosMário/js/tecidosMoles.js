844723c346111b5e72bdb0c9071e323f84cc188b
Graziela Souza <118382634+grazisou00@users.noreply.github.com>
2024-09-08 01:12:01 -0300

    Update tecidosMoles.js

--- PRODONTO/arquivosMário/js/tecidosMoles.js
+++ PRODONTO/arquivosMário/js/tecidosMoles.js
@@ -6,70 +6,92 @@ const modals = [
     'modal-sombra-da-gengiva'
 ];
 
+const modal123 = [
+    {
+        id: 'modal-projeo-do-lbio-superior',
+        images: ['/PRODONTO/img/tecidosMoles/tecimolmar/labiosup.jpeg', '/PRODONTO/img/tecidosMoles/Projecao\ do\ Labio\ Superior.png']
+    },
+    {
+        id: 'modal-projeo-do-lbio-inferior',
+        images: ['/PRODONTO/img/tecidosMoles/tecimolmar/labioinf.jpeg', '/PRODONTO/img/tecidosMoles/Projecao\ do\ labio\ Inferior.png']
+    },
+    {
+        id: 'modal-projeo-do-nariz',
+        images: ['/PRODONTO/img/tecidosMoles/tecimolmar/projnariz.jpeg', '/PRODONTO/img/tecidosMoles/Projecao\ do\ Nariz.png']
+    },
+    {
+        id: 'modal-projeo-do-sulco-nasolabial',
+        images: ['/PRODONTO/img/tecidosMoles/tecimolmar/geng-nasolab.jpeg', '/PRODONTO/img/tecidosMoles/Projecao\ do\ Sulco\ Nasolabial.png']
+    },
+    {
+        id: 'modal-sombra-da-gengiva',
+        images: ['/PRODONTO/img/tecidosMoles/tecimolmar/geng-nasolab.jpeg', '/PRODONTO/img/tecidosMoles/Gengiva.png']
+    }
+];
+  
+  
 let currentModalIndex = 0;
-let originalImage = '/PRODONTO/img/img223.jpeg';
-let compareImage = '/PRODONTO/img/img224.jpeg';
-
+  
 function abrirModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.classList.add("abrir");
-
+  
     const closeModal = () => {
         modal.classList.remove("abrir");
     };
-
+  
     modal.querySelector(".close-btn").addEventListener("click", closeModal);
     modal.addEventListener("click", (e) => {
         if (e.target.classList.contains('janela-modal-estrutura')) {
             closeModal();
         }
     });
-
-    // Atualizar o índice do modal atual
+  
+  
+      
     currentModalIndex = modals.indexOf(modalId);
-
-    // Adicionar event listeners ao modal atualmente aberto
-    const modalImage = modal.querySelector('#modal-image');
-    if (modalImage) {
-        modalImage.addEventListener('mouseover', function () {
-            this.src = compareImage;
-        });
-
-        modalImage.addEventListener('mouseout', function () {
-            this.src = originalImage;
-        });
-
-        modalImage.addEventListener('click', function () {
-            let temp = originalImage;
-            originalImage = compareImage;
-            compareImage = temp;
-            this.src = originalImage;
-        });
-    }
+    const modalImage = modal.querySelector('img');
+    let [originalImage, compareImage] = modal123[currentModalIndex].images;
+    modalImage.src = originalImage;
+  
+    
+    modalImage.addEventListener('mouseover', function () {
+        this.src = compareImage;
+    });
+  
+    modalImage.addEventListener('mouseout', function () {
+        this.src = originalImage;
+    });
+  
+    modalImage.addEventListener('click', function () {
+        let temp = originalImage;
+        originalImage = compareImage;
+        compareImage = temp;
+        this.src = originalImage;
+    });
 }
-
+  
 function previousModal() {
     const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
     if (currentModal) {
         currentModal.classList.remove('abrir');
     }
-
+  
     currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
     const previousModalId = modals[currentModalIndex];
     abrirModal(previousModalId);
 }
-
+  
 function nextModal() {
     const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
     if (currentModal) {
         currentModal.classList.remove('abrir');
     }
-
+  
     currentModalIndex = (currentModalIndex + 1) % modals.length;
     const nextModalId = modals[currentModalIndex];
     abrirModal(nextModalId);
 }
-
 // Abrir e fechar cada modal
 document.querySelector('.button-1').onclick = () => abrirModal('modal-projeo-do-lbio-superior');
 document.querySelector('.button-2').onclick = () => abrirModal('modal-projeo-do-lbio-inferior');
@@ -77,11 +99,6 @@ document.querySelector('.button-3').onclick = () => abrirModal('modal-projeo-do-
 document.querySelector('.button-4').onclick = () => abrirModal('modal-projeo-do-sulco-nasolabial');
 document.querySelector('.button-5').onclick = () => abrirModal('modal-sombra-da-gengiva');
 
-
-function changeImage(img1, img2) {
-    originalImage = img1;
-    compareImage = img2;
-}
 function abrirModalVm() {
     const modal = document.getElementById('janela-modal');
     modal.classList.add('abrir');
