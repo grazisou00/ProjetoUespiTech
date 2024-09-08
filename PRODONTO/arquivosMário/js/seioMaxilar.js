e1f7b04958b5c6beffad15664097247e451a63bf
Graziela Souza <118382634+grazisou00@users.noreply.github.com>
2024-09-08 01:11:58 -0300

    Update seioMaxilar.js

--- PRODONTO/arquivosMário/js/seioMaxilar.js
+++ PRODONTO/arquivosMário/js/seioMaxilar.js
@@ -5,66 +5,88 @@ const modals = [
     'modal-extenso-do-tber',
     'modal-septo-sinusal'
 ];
-
+const modal123 = [
+    {
+        id: 'modal-canais-nutrientes',
+        images: ['/PRODONTO/img/seioMaxilar/seiomar/canaisnutrientes.jpeg', '/PRODONTO/img/seioMaxilar/Canais\ Nutrientes.png']
+    },
+    {
+        id: 'modal-extenso-alveolar',
+        images: ['/PRODONTO/img/seioMaxilar/seiomar/extalve-septos.jpeg', '/PRODONTO/img/seioMaxilar/Extensao\ Alveolar.png']
+    },
+    {
+        id: 'modal-extenso-anterior',
+        images: ['/PRODONTO/img/seioMaxilar/seiomar/exteant.jpeg', '/PRODONTO/img/seioMaxilar/Extensao\ Anterior.png']
+    },
+    {
+        id: 'modal-extenso-do-tber',
+        images: ['/PRODONTO/img/seioMaxilar/seiomar/extptuber.jpeg', '/PRODONTO/img/seioMaxilar/Extensao\ para\ Tuber.png']
+    },
+    {
+        id: 'modal-septo-sinusal',
+        images: ['/PRODONTO/img/seioMaxilar/seiomar/extalve-septos.jpeg', '/PRODONTO/img/seioMaxilar/Septo\ Sinusal.png']
+    }
+];
+  
+  
 let currentModalIndex = 0;
-let originalImage = '/PRODONTO/img/img223.jpeg';
-let compareImage = '/PRODONTO/img/img224.jpeg';
-
+  
 function abrirModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.classList.add("abrir");
-
+  
     const closeModal = () => {
         modal.classList.remove("abrir");
     };
-
+  
     modal.querySelector(".close-btn").addEventListener("click", closeModal);
     modal.addEventListener("click", (e) => {
         if (e.target.classList.contains('janela-modal-estrutura')) {
             closeModal();
         }
     });
-
-    // Atualizar o índice do modal atual
+  
+  
+      
     currentModalIndex = modals.indexOf(modalId);
-
-    // Adicionar event listeners ao modal atualmente aberto
-    const modalImage = modal.querySelector('#modal-image');
-    if (modalImage) {
-        modalImage.addEventListener('mouseover', function () {
-            this.src = compareImage;
-        });
-
-        modalImage.addEventListener('mouseout', function () {
-            this.src = originalImage;
-        });
-
-        modalImage.addEventListener('click', function () {
-            let temp = originalImage;
-            originalImage = compareImage;
-            compareImage = temp;
-            this.src = originalImage;
-        });
-    }
+    const modalImage = modal.querySelector('img');
+    let [originalImage, compareImage] = modal123[currentModalIndex].images;
+    modalImage.src = originalImage;
+  
+    
+    modalImage.addEventListener('mouseover', function () {
+        this.src = compareImage;
+    });
+  
+    modalImage.addEventListener('mouseout', function () {
+        this.src = originalImage;
+    });
+  
+    modalImage.addEventListener('click', function () {
+        let temp = originalImage;
+        originalImage = compareImage;
+        compareImage = temp;
+        this.src = originalImage;
+    });
 }
-
+  
 function previousModal() {
     const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
     if (currentModal) {
         currentModal.classList.remove('abrir');
     }
-
+  
     currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
     const previousModalId = modals[currentModalIndex];
     abrirModal(previousModalId);
 }
-
+  
 function nextModal() {
     const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
     if (currentModal) {
         currentModal.classList.remove('abrir');
     }
-
+  
     currentModalIndex = (currentModalIndex + 1) % modals.length;
     const nextModalId = modals[currentModalIndex];
     abrirModal(nextModalId);
@@ -76,11 +98,6 @@ document.querySelector('.button-2').onclick = () => abrirModal('modal-extenso-al
 document.querySelector('.button-3').onclick = () => abrirModal('modal-extenso-anterior');
 document.querySelector('.button-4').onclick = () => abrirModal('modal-extenso-do-tber');
 document.querySelector('.button-5').onclick = () => abrirModal('modal-septo-sinusal');
-
-function changeImage(img1, img2) {
-    originalImage = img1;
-    compareImage = img2;
-}
 function abrirModalVm() {
     const modal = document.getElementById('janela-modal');
     modal.classList.add('abrir');
