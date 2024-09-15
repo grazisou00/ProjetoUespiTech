794844133aa812e673f3d9517864d0943e104bfe
mariogomess2312 <165229408+mariogomess2312@users.noreply.github.com>
2024-09-15 02:19:41 -0300

    Update incisivoSuperiores.js

--- PRODONTO/arquivosMário/js/incisivoSuperiores.js
+++ PRODONTO/arquivosMário/js/incisivoSuperiores.js
@@ -14,40 +14,40 @@ const modals = [
 
 const modal123 = [
   {
-      id: 'modal-sutura-palatina-mediana',
-      images: ['/PRODONTO/img/IncisivoSuperior/insuperma/suturapa-fossana.jpeg', '/PRODONTO/img/IncisivoSuperior/suturapalatinamedianahl.png']
+    id: 'modal-sutura-palatina-mediana',
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma/suturapa-fossana.jpeg', '/PRODONTO/img/IncisivoSuperior/suturapalatinamedianahl.png']
   },
   {
-      id: 'modal-fossas-nasais',
-      images: ['/PRODONTO/img/IncisivoSuperior/insuperma/suturapa-fossana.jpeg', '/PRODONTO/img/IncisivoSuperior/fossasnasais.png']
+    id: 'modal-fossas-nasais',
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma/suturapa-fossana.jpeg', '/PRODONTO/img/IncisivoSuperior/fossasnasais.png']
   },
   {
-      id: 'modal-forame-incisivo',
-      images: ['/PRODONTO/img/IncisivoSuperior/insuperma/forameinc-abena.jpeg', '/PRODONTO/img/IncisivoSuperior/forameincisivohl.png']
+    id: 'modal-forame-incisivo',
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma/forameinc-abena.jpeg', '/PRODONTO/img/IncisivoSuperior/forameincisivohl.png']
   },
   {
-      id: 'modal-canal-incisivo',
-      images: ['/PRODONTO/img/IncisivoSuperior/insuperma/canaisinsisivos.jpeg', '/PRODONTO/img/IncisivoSuperior/Canais\ Incisivos.png']
+    id: 'modal-canal-incisivo',
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma/canaisinsisivos.jpeg', '/PRODONTO/img/IncisivoSuperior/Canais\ Incisivos.png']
   },
   {
-      id: 'modal-aberturas-nasais-do-canal-incisivo',
-      images: ['/PRODONTO/img/IncisivoSuperior/insuperma//assoa-espi.jpeg', '/PRODONTO/img/IncisivoSuperior/espinhanasalanterior.png']
+    id: 'modal-aberturas-nasais-do-canal-incisivo',
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma//assoa-espi.jpeg', '/PRODONTO/img/IncisivoSuperior/espinhanasalanterior.png']
   },
   {
-      id: 'modal-fossas-incisivas',
-      images: ['/PRODONTO/img/IncisivoSuperior/insuperma/fossasin.jpeg', '/PRODONTO/img/areadeEstudo/IncisovoSuperiores/FossasIncisivas.png']
+    id: 'modal-fossas-incisivas',
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma/fossasin.jpeg', '/PRODONTO/img/areadeEstudo/IncisovoSuperiores/FossasIncisivas.png']
   },
   {
-      id: 'modal-espinha-nasal-do-anterior',
-      images: ['/PRODONTO/img/IncisivoSuperior/insuperma//assoa-espi.jpeg', '/PRODONTO/img/IncisivoSuperior/Aberturas\ Nasais.png']
+    id: 'modal-espinha-nasal-do-anterior',
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma//assoa-espi.jpeg', '/PRODONTO/img/IncisivoSuperior/Aberturas\ Nasais.png']
   },
   {
     id: 'modal-assoalho-de-fossas-nasais',
-    images: ['/PRODONTO/img/IncisivoSuperior/insuperma//assoa-espi.jpeg', '/PRODONTO/img/IncisivoSuperior/assoalhodefossanasal.png']
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma/assoalhodefossanasal1.png', '/PRODONTO/img/IncisivoSuperior/assoalhodefossanasal.png']
   },
   {
     id: 'modal-conchas-nasais-inferiores',
-    images: ['/PRODONTO/img/IncisivoSuperior/insuperma/concnasa.jpeg', '/PRODONTO/img/IncisivoSuperior/Conchas\ Nasais\ Inferiores.png']
+    images: ['/PRODONTO/img/IncisivoSuperior/insuperma/concnasa.png', '/PRODONTO/img/IncisivoSuperior/Conchas Nasais Inferiores.png']
   },
   {
     id: 'modal-septo-nasal',
@@ -63,64 +63,64 @@ const modal123 = [
 let currentModalIndex = 0;
 
 function abrirModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.classList.add("abrir");
+  const modal = document.getElementById(modalId);
+  modal.classList.add("abrir");
 
-    const closeModal = () => {
-        modal.classList.remove("abrir");
-    };
+  const closeModal = () => {
+    modal.classList.remove("abrir");
+  };
 
-    modal.querySelector(".close-btn").addEventListener("click", closeModal);
-    modal.addEventListener("click", (e) => {
-        if (e.target.classList.contains('janela-modal-estrutura')) {
-            closeModal();
-        }
-    });
+  modal.querySelector(".close-btn").addEventListener("click", closeModal);
+  modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains('janela-modal-estrutura')) {
+      closeModal();
+    }
+  });
 
 
-    // Adicionar event listeners ao modal atualmente aberto
-    currentModalIndex = modals.indexOf(modalId);
-    const modalImage = modal.querySelector('img');
-    let [originalImage, compareImage] = modal123[currentModalIndex].images;
-    modalImage.src = originalImage;
+  // Adicionar event listeners ao modal atualmente aberto
+  currentModalIndex = modals.indexOf(modalId);
+  const modalImage = modal.querySelector('img');
+  let [originalImage, compareImage] = modal123[currentModalIndex].images;
+  modalImage.src = originalImage;
 
-    // Adicionar event listeners ao modal atualmente aberto
-    modalImage.addEventListener('mouseover', function () {
-        this.src = compareImage;
-    });
+  // Adicionar event listeners ao modal atualmente aberto
+  modalImage.addEventListener('mouseover', function () {
+    this.src = compareImage;
+  });
 
-    modalImage.addEventListener('mouseout', function () {
-        this.src = originalImage;
-    });
+  modalImage.addEventListener('mouseout', function () {
+    this.src = originalImage;
+  });
 
-    modalImage.addEventListener('click', function () {
-        let temp = originalImage;
-        originalImage = compareImage;
-        compareImage = temp;
-        this.src = originalImage;
-    });
+  modalImage.addEventListener('click', function () {
+    let temp = originalImage;
+    originalImage = compareImage;
+    compareImage = temp;
+    this.src = originalImage;
+  });
 }
 
 function previousModal() {
-    const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
-    if (currentModal) {
-        currentModal.classList.remove('abrir');
-    }
+  const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
+  if (currentModal) {
+    currentModal.classList.remove('abrir');
+  }
 
-    currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
-    const previousModalId = modals[currentModalIndex];
-    abrirModal(previousModalId);
+  currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
+  const previousModalId = modals[currentModalIndex];
+  abrirModal(previousModalId);
 }
 
 function nextModal() {
-    const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
-    if (currentModal) {
-        currentModal.classList.remove('abrir');
-    }
+  const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
+  if (currentModal) {
+    currentModal.classList.remove('abrir');
+  }
 
-    currentModalIndex = (currentModalIndex + 1) % modals.length;
-    const nextModalId = modals[currentModalIndex];
-    abrirModal(nextModalId);
+  currentModalIndex = (currentModalIndex + 1) % modals.length;
+  const nextModalId = modals[currentModalIndex];
+  abrirModal(nextModalId);
 }
 
 
