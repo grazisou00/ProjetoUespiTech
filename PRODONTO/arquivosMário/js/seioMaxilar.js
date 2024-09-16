8e1042879278b4e6a0388a7511b4da4be84b6b21
mariogomess2312 <165229408+mariogomess2312@users.noreply.github.com>
2024-09-15 21:52:14 -0300

    Update seioMaxilar.js

--- PRODONTO/arquivosMário/js/seioMaxilar.js
+++ PRODONTO/arquivosMário/js/seioMaxilar.js
@@ -12,7 +12,7 @@ const modal123 = [
     },
     {
         id: 'modal-extenso-alveolar',
-        images: ['/PRODONTO/img/seioMaxilar/seiomar/extalve-septos.jpeg', '/PRODONTO/img/seioMaxilar/Extensao\ Alveolar.png']
+        images: ['/PRODONTO/img/seioMaxilar/seiomar/extensao_alveolar_sem_marca.png', '/PRODONTO/img/seioMaxilar/extensao_alveolar_marcada.png']
     },
     {
         id: 'modal-extenso-anterior',
@@ -24,44 +24,44 @@ const modal123 = [
     },
     {
         id: 'modal-septo-sinusal',
-        images: ['/PRODONTO/img/seioMaxilar/seiomar/extalve-septos.jpeg', '/PRODONTO/img/seioMaxilar/Septo\ Sinusal.png']
+        images: ['/PRODONTO/img/seioMaxilar/seiomar/septosinualsemmarca.png', '/PRODONTO/img/seioMaxilar/septosinualmarcado.png']
     }
 ];
-  
-  
+
+
 let currentModalIndex = 0;
-  
+
 function abrirModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.classList.add("abrir");
-  
+
     const closeModal = () => {
         modal.classList.remove("abrir");
     };
-  
+
     modal.querySelector(".close-btn").addEventListener("click", closeModal);
     modal.addEventListener("click", (e) => {
         if (e.target.classList.contains('janela-modal-estrutura')) {
             closeModal();
         }
     });
-  
-  
-      
+
+
+
     currentModalIndex = modals.indexOf(modalId);
     const modalImage = modal.querySelector('img');
     let [originalImage, compareImage] = modal123[currentModalIndex].images;
     modalImage.src = originalImage;
-  
-    
+
+
     modalImage.addEventListener('mouseover', function () {
         this.src = compareImage;
     });
-  
+
     modalImage.addEventListener('mouseout', function () {
         this.src = originalImage;
     });
-  
+
     modalImage.addEventListener('click', function () {
         let temp = originalImage;
         originalImage = compareImage;
@@ -69,24 +69,24 @@ function abrirModal(modalId) {
         this.src = originalImage;
     });
 }
-  
+
 function previousModal() {
     const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
     if (currentModal) {
         currentModal.classList.remove('abrir');
     }
-  
+
     currentModalIndex = (currentModalIndex - 1 + modals.length) % modals.length;
     const previousModalId = modals[currentModalIndex];
     abrirModal(previousModalId);
 }
-  
+
 function nextModal() {
     const currentModal = document.querySelector('.janela-modal-estrutura.abrir');
     if (currentModal) {
         currentModal.classList.remove('abrir');
     }
-  
+
     currentModalIndex = (currentModalIndex + 1) % modals.length;
     const nextModalId = modals[currentModalIndex];
     abrirModal(nextModalId);
